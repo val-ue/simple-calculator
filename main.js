8b46e1e22376e06eed181c34040546d24be1ce7e
val_l <contacttovalerie@gmail.com>
2025-03-07 02:24:21 -0500

    Condensed JS into one function

--- main.js
+++ main.js
@@ -1,65 +1,40 @@
 const get = function (selector) {
-    return document.querySelector(selector);
-  };
+  return document.querySelector(selector);
+};
 
-const firstNumber = get('.first-number');
-const secondNumber = get('.second-number');
-const addButton = get('.add'); 
-const subtractButton = get('.subtract'); 
-const divideButton = get('.divide'); 
-const multiplyButton = get('.multiply'); 
-const result = get('.result'); 
-const mathButtons = get('math-buttons');
+const firstNumber = get(".first-number");
+const secondNumber = get(".second-number");
+const addButton = get(".add");
+const subtractButton = get(".subtract");
+const divideButton = get(".divide");
+const multiplyButton = get(".multiply");
+const result = get(".result");
+const mathButtons = get("math-buttons");
 
+const buttons = document.querySelectorAll("button");
 
+const calculate = function (event) {
+  let firstInput = parseInt(document.getElementById("first-input").value);
+  let secondInput = parseInt(document.getElementById("second-input").value);
 
-const add = addButton.addEventListener('click', function() {
-    let firstInput = parseInt(document.getElementById('first-input').value);
-    let secondInput = parseInt(document.getElementById('second-input').value);
+  if (isNaN(firstInput) || isNaN(secondInput)) {
+    result.innerText = `Please use numbers only`;
+    return;
+  }
 
-    if (isNaN(firstInput) || isNaN(secondInput)) { 
-        result.innerText = `Please use numbers only`;
+  let buttonType = event.target.innerText;
 
-    } else {
-        result.innerText = `${firstInput + secondInput}`;
-    }
+  if (buttonType === "+") {
+    result.innerText = firstInput + secondInput;
+  } else if (buttonType === "-") {
+    result.innerText = firstInput - secondInput;
+  } else if (buttonType === "/") {
+    result.innerText = firstInput / secondInput;
+  } else {
+    result.innerText = firstInput * secondInput;
+  }
+};
+
+buttons.forEach(function (button) {
+  button.addEventListener("click", calculate);
 });
-
-const subtract = subtractButton.addEventListener('click', function() {
-    let firstInput = parseInt(document.getElementById('first-input').value);
-    let secondInput = parseInt(document.getElementById('second-input').value);
-
-    if (isNaN(firstInput) || isNaN(secondInput)) { 
-        result.innerText = `Please use numbers only`;
-
-    } else {
-        result.innerText = `${firstInput - secondInput}`;
-    }
-});
-
-const divide = divideButton.addEventListener('click', function() {
-    let firstInput = parseInt(document.getElementById('first-input').value);
-    let secondInput = parseInt(document.getElementById('second-input').value);
-
-    if (isNaN(firstInput) || isNaN(secondInput)) { 
-        result.innerText = `Please use numbers only`;
-
-    } else {
-        result.innerText = `${firstInput / secondInput}`;
-    }
-});
-
-const multiply = multiplyButton.addEventListener('click', function() {
-    let firstInput = parseInt(document.getElementById('first-input').value);
-    let secondInput = parseInt(document.getElementById('second-input').value);
-
-    if (isNaN(firstInput) || isNaN(secondInput)) { 
-        result.innerText = `Please use numbers only`;
-
-    } else {
-        result.innerText = `${firstInput * secondInput}`;
-    }
-});
-
-
-
