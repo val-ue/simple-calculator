aee7aba41fd7d21c9168e3dd2b4ae5c9c33a9fa5
val_l <contacttovalerie@gmail.com>
2025-03-09 23:59:14 -0400

    Added number validator function

--- main.js
+++ main.js
@@ -9,20 +9,49 @@ const subtractButton = get(".subtract");
 const divideButton = get(".divide");
 const multiplyButton = get(".multiply");
 const result = get(".result");
-const mathButtons = get("math-buttons");
+const firstInput = get("#first-input");
+const secondInput = get("#second-input");
 
 const buttons = document.querySelectorAll("button");
 
+firstInput.addEventListener("input", function() {
+  if (isNaN(parseInt(firstInput.value))) {
+    firstInput.style.boxShadow = "0px 0px 5px rgb(240, 61, 61)"; 
+    firstInput.style.backgroundColor = "rgb(250, 213, 255)";
+  } else {
+    firstInput.style.boxShadow = "0px 0px 5px #61c5fa"; 
+    firstInput.style.backgroundColor = "white"; 
+  }
+});
+
+const validate = function (input) {
+  if (isNaN(parseInt(input.value))) {
+    input.style.boxShadow = "0px 0px 5px rgb(240, 61, 61)"; 
+    input.style.backgroundColor = "rgb(250, 213, 255)";
+  } else {
+    input.style.boxShadow = "0px 0px 5px #61c5fa"; 
+    input.style.backgroundColor = "white"; 
+  }
+};
+
+firstInput.addEventListener("input", function() {
+  validate(firstInput);
+});
+
+secondInput.addEventListener("input", function() {
+  validate(secondInput);
+});
+
 const calculate = function (event) {
-  let firstInput = parseInt(get("#first-input").value);
-  let secondInput = parseInt(get("#second-input").value);
+  const firstInput = parseInt(get("#first-input").value);
+  const secondInput = parseInt(get("#second-input").value);
 
   if (isNaN(firstInput) || isNaN(secondInput)) {
     result.innerText = `Please use numbers only`;
     return;
   }
 
-  let buttonType = event.target.innerText;
+  const buttonType = event.target.innerText;
 
   if (buttonType === "+") {
     result.innerText = firstInput + secondInput;
